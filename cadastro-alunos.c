c8e05adac3c281f5732244096ae0f4b6c6923f02
ana.drmidnight@gmail.com <ana.drmidnight@gmail.com>
2026-03-08 13:43:59 -0300

    Criando algoritmo básico

---
+++ cadastro-alunos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    int idade;
+
+    printf("Digite o nome do aluno: ");
+    fgets(nome, sizeof(nome), stdin);
+
+    printf("Digite a idade do aluno: ");
+    scanf("%d", &idade);
+
+    printf("Aluno cadastrado: %sIdade: %d\n", nome, idade);
+
+    return 0;
+}
